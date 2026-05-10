bf246acfddf5cda535ede1651ad5bb57a7751c68
azizasadov935-hue <azizasadov935@gmail.com>
2026-05-10 08:35:19 +0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
+
+export default withNextIntl(nextConfig);
